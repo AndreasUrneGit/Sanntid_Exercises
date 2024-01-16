e77b6ce67dad7793a5425a57be2d013138da2459
Andreas Urne <urneandreas@gmail.com>
2024-01-16 17:40:54 +0100

    Exercise 1 1, Task 5

--- Exercise_1/bounded buffer/go/main.go
+++ Exercise_1/bounded buffer/go/main.go
@@ -5,21 +5,22 @@ import "fmt"
 import "time"
 
 
-func producer(/*TODO: parameters?*/){
+func producer(bufferedChan chan<- int){
 
     for i := 0; i < 10; i++ {
         time.Sleep(100 * time.Millisecond)
         fmt.Printf("[producer]: pushing %d\n", i)
         // TODO: push real value to buffer
+        bufferedChan <- i
     }
 
 }
 
-func consumer(/*TODO: parameters?*/){
+func consumer(bufferedChan <-chan int){
 
     time.Sleep(1 * time.Second)
     for {
-        i := 0 //TODO: get real value from buffer
+        i := <- bufferedChan //TODO: get real value from buffer
         fmt.Printf("[consumer]: %d\n", i)
         time.Sleep(50 * time.Millisecond)
     }
@@ -30,9 +31,9 @@ func consumer(/*TODO: parameters?*/){
 func main(){
     
     // TODO: make a bounded buffer
-    
-    go consumer()
-    go producer()
+    bufferedChan := make(chan int, 5)
+    go consumer(bufferedChan)
+    go producer(bufferedChan)
     
     select {}
 }
