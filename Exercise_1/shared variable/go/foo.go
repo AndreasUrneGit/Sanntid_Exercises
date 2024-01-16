bd40e1c143b05d129d658fc4c6d949f6b980b889
Andreas Urne <urneandreas@gmail.com>
2024-01-16 17:18:54 +0100

    Exercise 1, Task 4

--- Exercise_1/shared variable/go/foo.go
+++ Exercise_1/shared variable/go/foo.go
@@ -3,45 +3,57 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
+	//"time"
 )
 
 var i = 0
 
-func incrementing() {
-    //TODO: increment i 1000000 times
-    i += 1
+func i_handle(incrChan <-chan bool, decrChan <-chan bool, done <-chan bool) {
+	//TODO: increment i 1000000 times
+	for {
+		select {
+		case <-incrChan:
+			i += 1
+		case <-decrChan:
+			i -= 1
+		case <-done:
+			return
+		}
+	}
 }
 
-func decrementing() {
-    //TODO: decrement i 1000000 times
-    i -= 1
+func thread_1(incrChan chan<- bool, done chan<- bool) {
+	for j := 0; j < 1000000; j++ {
+		incrChan <- true
+	}
+	done <- true
 }
-
-func thread_1(){
-    for j := 0; j < 1000000; j++{
-        incrementing()
-    }
-}
-func thread_2(){
-    for k := 0; k < 1000000; k++{
-        decrementing()
-    }
+func thread_2(decrChan chan<- bool, done chan<- bool) {
+	for k := 0; k < 999999; k++ {
+		decrChan <- true
+	}
+	done <- true
 }
 
 func main() {
-    // What does GOMAXPROCS do? What happens if you set it to 1?
-    runtime.GOMAXPROCS(2)    
-	
-    // TODO: Spawn both functions as goroutines
-    //spawn thread_1
-    go thread_1()
-    //spawn stread_2
-	go thread_2()
-    // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
-    // We will do it properly with channels soon. For now: Sleep.
-    time.Sleep(500*time.Millisecond)
-    Println("The magic number is:", i)
+	// What does GOMAXPROCS do? What happens if you set it to 1?
+	runtime.GOMAXPROCS(3)
+	// TODO: Spawn both functions as goroutines
+	incrChan := make(chan bool)
+	decrChan := make(chan bool)
+	done := make(chan bool)
+	//spawn thread_1
+	go i_handle(incrChan, decrChan, done)
+	go thread_1(incrChan, done)
+	//spawn stread_2
+	go thread_2(decrChan, done)
+	// We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
+	// We will do it properly with channels soon. For now: Sleep.
+	<-done
+	<-done
+	done <- true
+	//time.Sleep(500 * time.Millisecond)
+	Println("The magic number is:", i)
 }
